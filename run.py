e9364137f2d46589794389be9fa5974f801ba403
vittoriomaisano <108636033+vittoriomaisano@users.noreply.github.com>
2024-09-14 14:43:24 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,19 +1,5 @@
 import re
 
-TEXT = r'''
-
- ▄▄▄▄   ▓█████  ██▓     ▒█████   ███▄ ▄███▓ ▒█████   ██▀███  
-▓█████▄ ▓█   ▀ ▓██▒    ▒██▒  ██▒▓██▒▀█▀ ██▒▒██▒  ██▒▓██ ▒ ██▒
-▒██▒ ▄██▒███   ▒██░    ▒██░  ██▒▓██    ▓██░▒██░  ██▒▓██ ░▄█ ▒
-▒██░█▀  ▒▓█  ▄ ▒██░    ▒██   ██░▒██    ▒██ ▒██   ██░▒██▀▀█▄  
-░▓█  ▀█▓░▒████▒░██████▒░ ████▓▒░▒██▒   ░██▒░ ████▓▒░░██▓ ▒██▒
-░▒▓███▀▒░░ ▒░ ░░ ▒░▓  ░░ ▒░▒░▒░ ░ ▒░   ░  ░░ ▒░▒░▒░ ░ ▒▓ ░▒▓░
-▒░▒   ░  ░ ░  ░░ ░ ▒  ░  ░ ▒ ▒░ ░  ░      ░  ░ ▒ ▒░   ░▒ ░ ▒░
- ░    ░    ░     ░ ░   ░ ░ ░ ▒  ░      ░   ░ ░ ░ ▒    ░░   ░ 
- ░         ░  ░    ░  ░    ░ ░         ░       ░ ░     ░
-
-'''
-
 def get_wallet_addresses():
     print(TEXT)
     print("Вставь кошельки построчно и нажми ENTER:\n")
@@ -25,20 +11,40 @@ def get_wallet_addresses():
         addresses.append(line)
     return addresses
 
-wallet_addresses = get_wallet_addresses()
+def update_console_js(wallet_addresses):
+    wallets_js = ', '.join([f'"{address}"' for address in wallet_addresses])
+    new_wallets_line = f'const wallets = [{wallets_js}];'
 
-wallets_js = ', '.join([f'"{address}"' for address in wallet_addresses])
-new_wallets_line = f'const wallets = [{wallets_js}];'
+    try:
+        with open('console.js', 'r') as f:
+            existing_code = f.read()
+    except FileNotFoundError:
+        print("Файл console.js не найден.")
+        exit(1)
 
-try:
-    with open('console.js', 'r') as f:
-        existing_code = f.read()
-except FileNotFoundError:
-    print("Файл console.js не найден.")
-    exit(1)
+    pattern = re.compile(r'const wallets = \[.*?\];', re.DOTALL)
+    updated_code = pattern.sub(new_wallets_line, existing_code)
 
-pattern = re.compile(r'const wallets = \[.*?\];', re.DOTALL)
-updated_code = pattern.sub(new_wallets_line, existing_code)
+    print("Открой консоль на сайте https://www.okx.com/ru/balance/withdrawal-add-address/btc и вставь туда этот код:\n\n\n\n")
+    print(updated_code)
+    print('\n\n')
 
-print("\nВот обновленный код с адресами:\nОткрой консоль на сайте https://www.okx.com/ru/balance/withdrawal-add-address/btc и вставь его туда.\n")
-print(updated_code)
+if __name__ == "__main__":
+
+    TEXT = r'''
+
+    
+    ▄▄▄▄   ▓█████  ██▓     ▒█████   ███▄ ▄███▓ ▒█████   ██▀███  
+    ▓█████▄ ▓█   ▀ ▓██▒    ▒██▒  ██▒▓██▒▀█▀ ██▒▒██▒  ██▒▓██ ▒ ██▒
+    ▒██▒ ▄██▒███   ▒██░    ▒██░  ██▒▓██    ▓██░▒██░  ██▒▓██ ░▄█ ▒
+    ▒██░█▀  ▒▓█  ▄ ▒██░    ▒██   ██░▒██    ▒██ ▒██   ██░▒██▀▀█▄  
+    ░▓█  ▀█▓░▒████▒░██████▒░ ████▓▒░▒██▒   ░██▒░ ████▓▒░░██▓ ▒██▒
+    ░▒▓███▀▒░░ ▒░ ░░ ▒░▓  ░░ ▒░▒░▒░ ░ ▒░   ░  ░░ ▒░▒░▒░ ░ ▒▓ ░▒▓░
+    ▒░▒   ░  ░ ░  ░░ ░ ▒  ░  ░ ▒ ▒░ ░  ░      ░  ░ ▒ ▒░   ░▒ ░ ▒░
+    ░    ░    ░     ░ ░   ░ ░ ░ ▒  ░      ░   ░ ░ ░ ▒    ░░   ░ 
+    ░         ░  ░    ░  ░    ░ ░         ░       ░ ░     ░
+
+    
+    '''
+    wallet_addresses = get_wallet_addresses()
+    update_console_js(wallet_addresses)
