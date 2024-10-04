f8ea2044deea2b08cedceb340c0830f42d98c935
vittoriomaisano <108636033+vittoriomaisano@users.noreply.github.com>
2024-10-05 01:07:52 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 import re
 
 def get_wallet_addresses():
-    print(TEXT)
     print("Вставь кошельки построчно и нажми ENTER:\n")
     addresses = []
     while True:
@@ -11,23 +10,35 @@ def get_wallet_addresses():
         addresses.append(line)
     return addresses
 
-def update_console_js(wallet_addresses):
+def update_console_js(wallet_addresses, filename):
     wallets_js = ', '.join([f'"{address}"' for address in wallet_addresses])
     new_wallets_line = f'const wallets = [{wallets_js}];'
 
     try:
-        with open('console.js', 'r') as f:
+        with open(filename, 'r') as f:
             existing_code = f.read()
     except FileNotFoundError:
-        print("Файл console.js не найден.")
+        print(f"Файл {filename} не найден.")
         exit(1)
 
     pattern = re.compile(r'const wallets = \[.*?\];', re.DOTALL)
     updated_code = pattern.sub(new_wallets_line, existing_code)
 
-    print("Открой консоль на сайте https://www.okx.com/ru/balance/withdrawal-add-address/btc и вставь туда этот код:\n\n\n\n")
+    print(f"Открой консоль на сайте https://www.okx.com/ru/balance/withdrawal-add-address и вставь туда этот код:\n\n\n")
     print(updated_code)
     print('\n\n')
+
+def select_module():
+    print("Выберите модуль для запуска:")
+    print("\n1. BTC-Bitcoin")
+    print("2. SATS-Bitcoin\n")
+
+    while True:
+        choice = input("Введите 1 или 2: ").strip()
+        if choice in ['1', '2']:
+            return choice
+        else:
+            print("Неверный ввод. Пожалуйста, введите 1 или 2.")
 
 if __name__ == "__main__":
 
@@ -46,5 +57,14 @@ if __name__ == "__main__":
 
     
     '''
+
+    print(TEXT)
+    module_choice = select_module()
+
+    if module_choice == '1':
+        filename = 'console.js'
+    else:
+        filename = 'console_sats.js'
+
     wallet_addresses = get_wallet_addresses()
-    update_console_js(wallet_addresses)
+    update_console_js(wallet_addresses, filename)
